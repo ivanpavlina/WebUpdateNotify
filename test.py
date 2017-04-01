70a2bbb87b6ff111c9d98bbd48d76b11d1e70e4f
Ivan Pavlina <ivanpavlina@users.noreply.github.com>
2017-04-01 18:39:37 +0200

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-str = 'http://hns-cff.hr/files/documents/11815/01. Parova,sudaca,delegata 14. kolo.pdf'
-res = ''
-
-for char in str:
-    if char == ' ':
-        char = '%20'
-    res += char
-
-print res
